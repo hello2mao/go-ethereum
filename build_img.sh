5573cc4ae9971d1be43bbf3499c276bf2329a87a
hello2mao <hello2mao@gmail.com>
2019-04-12 13:28:38 +0800

    change docker images to client-go:stable

--- build_img.sh
+++ build_img.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION=v1.8.12-opt
-docker rmi -f hub.baidubce.com/jpaas-public/geth:${VERSION}
-docker build -t hub.baidubce.com/jpaas-public/geth:${VERSION} .
-docker push hub.baidubce.com/jpaas-public/geth:${VERSION}
+VERSION=stable
+docker rmi -f hub.baidubce.com/jpaas-public/client-go:${VERSION}
+docker build -t hub.baidubce.com/jpaas-public/client-go:${VERSION} .
+docker push hub.baidubce.com/jpaas-public/client-go:${VERSION}
